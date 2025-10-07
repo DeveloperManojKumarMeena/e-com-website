1df4d5bf6dd803ba314e38c7a745d12d8135f214
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2025-10-07 17:49:49 +0530

    axios and folder str complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import axios from 'axios';
+import React, { use, useEffect } from 'react'
 
 const App = () => {
+const product = async () => {
+  try {
+    let products = await axios.get('/products');
+    console.log('Products:', products);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+}
+useEffect(() => {
+  product();
+}, []);
+
   return (
     <div>App</div>
   )
