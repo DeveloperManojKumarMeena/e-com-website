d3c388f66bc19cab9766a854f98cf986cb7d7060
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2025-10-09 15:18:17 +0530

    log form hook

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import {useForm} from 'react-hook-form'
+import { NavLink } from 'react-router-dom';
 
 
 const Login = () => {
+const {register, handleSubmit,reset} = useForm();
+
+const LoginHandler = (data) => {
+  console.log(data);
+} 
   return (
-    <div class='bg-image w-screen h-screen flex justify-center items-center p-10'>
+    <div className='bg-image w-screen h-screen flex justify-center items-center p-10'>
 
       <div className='w-full h-full bg-black  rounded-lg flex justify-center items-center gap-10 glassmorphism'>
       <div className='w-1/2 h-[90%] flex flex-col justify-center items-center  '>
         <h1 className='text-4xl font-bold mb-8 text-white'>Login</h1>
-        <form className='flex flex-col gap-4 w-3/4'>
+
+
+        <form className='flex flex-col gap-4 w-3/4' onSubmit={handleSubmit(LoginHandler)}>
           <input 
             type="email"
             placeholder="Email"
             className='p-2 rounded-lg border border-gray-300'
+            {...register("email", { required: true })}  
           />
           <input 
             type="password"
             placeholder="Password"
             className='p-2 rounded-lg border border-gray-300'
+            {...register("password", { required: true })}
           />
           <button 
             type="submit"
@@ -40,7 +50,7 @@ const Login = () => {
           loop
           autoplay
         />
-        <p className='text-center text-white mb-10'>Not A Member? Register</p>
+        <p className='text-center text-white mb-10'>Not A Member? <NavLink to="/register" className='text-blue-500'>Register</NavLink></p>
      </div>
      </div>
     </div>
--- frontend/src/routes/Mainroutes.jsx
+++ frontend/src/routes/Mainroutes.jsx
@@ -3,6 +3,7 @@ import Home from '../pages/Home'
 import { Routes, Route } from 'react-router-dom'
 import Products from '../pages/Products'
 import Login from '../pages/Login'
+import Register from '../pages/Register'
 
 const Mainroutes = () => {
   return (
@@ -10,6 +11,8 @@ const Mainroutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
     </Routes>
   )
 }
